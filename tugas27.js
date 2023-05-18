09545ae524409a96b09387ad0369aaacc1244d99
AirPutih19 <kharismadwisj@gmail.com>
2023-05-19 06:41:43 +0700

    Tugas 27 JS
    Tugas 27 JS untuk niomic

---
+++ tugas27.js
@@ -0,0 +1,26 @@
+function panggilAlamat(){
+    var a = "No";
+    var b = "rumah saya"
+    var c = 30;
+    var d = "A"
+
+    hasil1 = a+(" ")+b+(" ")+String(c)+d;
+    console.log(hasil1)
+
+    var e = "Jalan pangeran Tirtayasa No";
+    var f = 70;
+    
+    hasil2 = e +(" ")+String(f);
+    console.log(hasil2)
+
+
+
+    
+
+   
+
+
+
+
+}
+panggilAlamat()
